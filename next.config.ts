3d9f76027d8ccf44b772e1ae26de85eec0ec8796
muhriddin20056525 <muhriddindavlatov89@gmail.com>
2025-03-11 10:12:33 +0500

    chnage next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
